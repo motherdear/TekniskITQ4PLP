5c524c01064c90c91fb164539d7a3d674bd14d1f
motherdear <christofferrclausen@gmail.com>
2016-04-15 11:09:11 +0200

    Updated the SchemeHandler to be an interface.

--- FEvaluator/FEvaluator/MainWindow.xaml.cs
+++ FEvaluator/FEvaluator/MainWindow.xaml.cs
@@ -12,53 +12,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Scheme;
 
-namespace FEvaluator
-{
-    using IronScheme; // the extension methods are exported from this namespace
-    public class SchemeHandler
-    {
-        public SchemeHandler()
-        {
-            // Hacky Hack HACK
-            // Evaluate a non defining statement in scheme to bootstrap the loading of the underlying evaluater, otherwise the first calculation will be slow.
-            Evaluate("(+)");
-        }
-        public object Evaluate(string input)
-        {
-            return input.Eval(); // calls IronScheme.RuntimeExtensions.Eval(string)
-        }
-    }
-}
+
 namespace FEvaluator
 {
     using System.Windows;
-
     public partial class Window1 : Window
     {
-        private SchemeHandler schemeHandler;
+        private IIronScheme SchemeScript;
         public Window1()
         {
             InitializeComponent();
             // Instantiate scheme
-            schemeHandler = new SchemeHandler();        }
+            SchemeScript = new SchemeHandler();        
+        }
 
         private void Evaluate_Click(object sender, RoutedEventArgs e)
         {
-            DisplayArea.Text = schemeHandler.Evaluate(Input.Text).ToString();
+            DisplayArea.Text = SchemeScript.EvalToString(Input.Text);
         }
 
         private void DefinitionTests() 
         {
-            var r1 = schemeHandler.Evaluate("(+ 1 2)");  // r1 is 3 
+            var r1 = SchemeScript.Eval("(+ 1 2)");  // r1 is 3 
 
-            var r2 = schemeHandler.Evaluate("(+ {0} {1})");  // r2 is 3
+            var r2 = SchemeScript.Eval("(+ {0} {1})");  // r2 is 3
 
-            var myproc = schemeHandler.Evaluate("(lambda (x y) (+ x y))");
-            var r3 = schemeHandler.Evaluate("({0} {1} {2})"); // r3 is 3
+            var myproc = SchemeScript.Eval("(lambda (x y) (+ x y))");
+            var r3 = SchemeScript.Eval("({0} {1} {2})"); // r3 is 3
 
-            schemeHandler.Evaluate("(define foo 1000)");  // executes definition in interaction environment
-            var r4 = schemeHandler.Evaluate("foo"); // r4 is 1000
+            SchemeScript.Eval("(define foo 1000)");  // executes definition in interaction environment
+            var r4 = SchemeScript.Eval("foo"); // r4 is 1000
         }
     }
 }
---
+++ FEvaluator/FEvaluator/SchemeHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheme
+{
+    using IronScheme; // the extension methods are exported from this namespace
+    public interface IIronScheme
+    {
+        string EvalToString(string expr);
+        string EvalToString(string expr, string importspec);
+        object Eval(string expr, params object[] args);
+        object Eval(string expr, string importspec, params object[] args);
+    }
+    public class SchemeHandler : IIronScheme
+    {
+        public SchemeHandler()
+        {
+            // Hacky Hack HACK
+            // Evaluate a non defining statement in scheme to bootstrap the loading of the underlying evaluater, otherwise the first calculation will be slow.
+            Eval("(+)");
+        }
+        public object Eval(string expr, params object[] args)
+        {
+            return expr.Eval(); // calls IronScheme.RuntimeExtensions.Eval(string)
+        }
+
+        public object Eval(string expr, string importspec, params object[] args)
+        {
+            return expr.Eval();
+        }
+
+        public string EvalToString(string expr) 
+        {
+            return expr.Eval().ToString();
+        }
+
+        public string EvalToString(string expr, string importspec)
+        {
+            return expr.Eval().ToString();
+        }
+    }
+    
+}
